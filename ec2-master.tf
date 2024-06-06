a78c5e032d073593d7dc9a042af0caa59e26d4a5
Pierren <pierrejeandeveloper@gmail.com>
2024-06-06 00:21:49 -0400

    Adding one master node and one worker node to the cluster

--- ec2-master.tf
+++ ec2-master.tf
@@ -1,4 +1,4 @@
-resource  "aws_instance" "ec2-control" {
+resource  "aws_instance" "ec2-control1" {
     ami = "ami-0e001c9271cf7f3b9" # ubuntu image 22.04
     instance_type = "t2.medium"
     subnet_id = aws_subnet.public-subnet.id
@@ -6,9 +6,22 @@ resource  "aws_instance" "ec2-control" {
     key_name = "cluster-k8s"
 
     tags = {
-        "Name" = "ec2-control"
+        "Name" = "ec2-control1"
     }
 }
+
+resource  "aws_instance" "ec2-control2" {
+    ami = "ami-0e001c9271cf7f3b9" # ubuntu image 22.04
+    instance_type = "t2.medium"
+    subnet_id = aws_subnet.public-subnet.id
+    vpc_security_group_ids = [aws_security_group.custom-sg.id]
+    key_name = "cluster-k8s"
+
+    tags = {
+        "Name" = "ec2-control2"
+    }
+}
+
 
 resource  "aws_instance" "ec2-worker1" {
     ami = "ami-0e001c9271cf7f3b9" # ubuntu image 22.04
@@ -34,6 +47,17 @@ resource  "aws_instance" "ec2-worker2" {
     }
 }
 
+resource  "aws_instance" "ec2-worker3" {
+    ami = "ami-0e001c9271cf7f3b9" # ubuntu image 22.04
+    instance_type = "t2.medium"
+    subnet_id = aws_subnet.public-subnet.id
+    vpc_security_group_ids = [aws_security_group.custom-sg.id]
+    key_name = "cluster-k8s"
+
+    tags = {
+        "Name" = "ec2-worker3"
+    }
+}
 
 
 
