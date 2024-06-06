2d8608b54c2172100f2b01c4d29b962711f57856
pierre <154103550+pierrejean509@users.noreply.github.com>
2024-06-06 11:49:01 -0400

    Delete route-table-association.tf

--- route-table-association.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route_table_association" "custom-rt-internet-association" {
-    subnet_id = aws_subnet.public-subnet.id
-    route_table_id = aws_route_table.custom-rt-internet.id 
-}
